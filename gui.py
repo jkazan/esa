cd86ec9e99a592bee5c45b4679676780468d88e5
Johannes Kazantzidis <johannes.kazantzidis@axis.com>
2020-07-07 14:18:33 +0200

    test

--- gui.py
+++ gui.py
@@ -19,7 +19,6 @@ from PyQt5.QtWidgets import (QMainWindow,
                              QCheckBox)
 
 class App(QMainWindow):
-
     def __init__(self):
         super().__init__()
         self.title = 'GUI'
